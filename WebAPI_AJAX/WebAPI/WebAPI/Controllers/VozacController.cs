15ac1d11dac526a17e9914cbd00451964ad5f796
Aleksa Gligoric <aleksa1996@hotmail.com>
2018-06-21 20:50:07 +0200

    Vozac logovanje

---
+++ WebAPI_AJAX/WebAPI/WebAPI/Controllers/VozacController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class VozacController : ApiController
+    {
+        public List<Vozac> Get()
+        {
+            Vozaci vozaci = (Vozaci)HttpContext.Current.Application["vozaci"];
+            List<Vozac> lista = new List<Vozac>();
+
+            foreach (var v in vozaci.list)
+                lista.Add(v.Value);
+
+            return lista;
+        }
+
+        public Vozac Get(string id)
+        {
+            Vozaci vozaci = (Vozaci)HttpContext.Current.Application["vozaci"];
+            return vozaci.list[id];
+        }
+
+    }
+}
